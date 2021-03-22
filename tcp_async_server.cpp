3a137e06074fee95019f6009b133e73f908d26c4
colin mcgovern <colinmcg@udel.edu>
2021-03-22 09:47:53 -0400

    update

--- tcp_async_server.cpp
+++ tcp_async_server.cpp
@@ -10,47 +10,80 @@
 
 using boost::asio::ip::tcp;
 using namespace std;
+using std::vector;
+
+// std::string message_ = "hello";
+
+struct client_t {
+	string ip_address;
+	unsigned short port;
+} typedef client_t;
+
+vector<client_t> clients;
 
 class tcp_connection : public boost::enable_shared_from_this<tcp_connection> {
 	public: 
 		typedef boost::shared_ptr<tcp_connection> pointer;
 
 		static pointer create(boost::asio::io_context& io_context){
+			cout << "tcp_connection::pointer()" << endl; //del
 			return pointer(new tcp_connection(io_context));
 		}
 
 		tcp::socket& socket(){
+			cout << "socket()" << endl; //del
 			return socket_;
 		}
 
 		void start(){
-				cout << "[SERVER] New Connection: ";
+			cout << "start() started" << endl; //del
 
-				for(;;){
-					message_ = "hello";
+			cout << "Sending info to " <<
+			 socket_.remote_endpoint().address().to_string() << ":" << 
+			 socket_.remote_endpoint().port() <<
+			endl;
 
-				boost::asio::async_write(socket_, boost::asio::buffer(message_),
-		        boost::bind(&tcp_connection::handle_write, shared_from_this(),
-		          boost::asio::placeholders::error,
-		          boost::asio::placeholders::bytes_transferred));
-				}
-				
+			client_t new_client = {
+				socket_.remote_endpoint().address().to_string(),
+				socket_.remote_endpoint().port() 
+			};
+
+			clients.push_back(new_client);
+
+			// const size_t id = clients.size();
+
+			// boost::asio::async_write(socket_, boost::asio::buffer(&id,sizeof(size_t)),
+	  //       boost::bind(&tcp_connection::handle_write, shared_from_this(),
+	  //         boost::asio::placeholders::error,
+	  //         boost::asio::placeholders::bytes_transferred));
+
+			int x = 1;
+
+			printf("Value:  %p\n", &x );
+
+			boost::asio::async_write(socket_, boost::asio::buffer(x,sizeof(int*)),
+			boost::bind(&tcp_connection::handle_write, shared_from_this(),
+			boost::asio::placeholders::error,
+			boost::asio::placeholders::bytes_transferred));
+
 			
+			cout << "start() ended" << endl; //del
 		}
 
 	private:
 		tcp_connection(boost::asio::io_context& io_context)
 		: socket_(io_context)
 		{
+			cout << "tcp_connection()" << endl; //del
 		}
 
 		void handle_write(const boost::system::error_code& /*error*/,
 		  size_t /*bytes_transferred*/)
 		{
+			cout << "handle_write()" << endl; //del
 		}
 
 		tcp::socket socket_;
-		std::string message_;
 };
 
 
@@ -58,28 +91,39 @@ class tcp_server{
 	public:
 		tcp_server(boost::asio::io_context& io_context) : io_context_(io_context), 
 		acceptor_(io_context, tcp::endpoint(tcp::v4(), 15)){
+			cout << "tcp_server() started" << endl; //del
 			start_accept();
+			cout << "tcp_server() ended" << endl; //del
 		}
 
 	private:
 		void start_accept(){
+
+			// cout << "enter message:";
+			// cin >> message_;
+
+			cout << "start_accept() started" << endl; //del
 			tcp_connection::pointer new_connection = tcp_connection::create(io_context_);
 
 			acceptor_.async_accept(new_connection->socket(),
 				boost::bind(&tcp_server::handle_accept, this, new_connection,
 					boost::asio::placeholders::error));
 
-
+			cout << "start_accept() ended" << endl; //del
 		}
 
 		void handle_accept(tcp_connection::pointer new_connection,
 			const boost::system::error_code& error){
+
+			cout << "handle_accept() started" << endl; //del
 
 			if(!error){
 				new_connection->start();
 			}
 
 			start_accept();
+
+			cout << "handle_accept() ended" << endl; //del
 		}
 
 		boost::asio::io_context& io_context_;
@@ -89,17 +133,20 @@ class tcp_server{
 
 int main(){
 
-	cout << "started" << endl;
+	cout << "main() started" << endl; //del
 
-	try{
-		boost::asio::io_context io_context;
-		tcp_server server(io_context);
-		io_context.run();
-	}catch(std::exception& e){
-		std::cerr << e.what() << std::endl;
+	for(;;){
+		
+		try{
+			boost::asio::io_context io_context;
+			tcp_server server(io_context);
+			io_context.run();
+		}catch(std::exception& e){
+			std::cerr << e.what() << std::endl;
+		}
 	}
 
-	cout << "ended" << endl;
+	cout << "main() ended" << endl; //del
 
 	return 0;
 }
--- tcp_client.cpp
+++ tcp_client.cpp
@@ -18,31 +18,53 @@ int main(int argc, char* argv[]){
 		cerr << e.what() << endl;
 	}
 
+	cout << "io_context" << endl; //del
 	boost::asio::io_context io_context;
+	cout << "resolver" << endl; //del
 	tcp::resolver resolver(io_context);
 
+	cout << "endpoints" << endl; //del
 	tcp::resolver::results_type endpoints = resolver.resolve(argv[1], "15");
 
+	cout << "socket" << endl; //del
 	tcp::socket socket(io_context);
+	cout << "connect" << endl; //del
 	boost::asio::connect(socket, endpoints);
 
 	for(;;){
-		boost::array<char,128> buf;
+
+		cout << "loop start" << endl; //del
+
+		// boost::array<char,128> buf;
+		boost::array<int,1> buf;
 		boost::system::error_code error;
 
 		size_t len = socket.read_some(boost::asio::buffer(buf), error);
 
-		// if(error == boost::asio::error::eof){
-		// 	cout << 2 << endl;
-		// 	break;
-		// }else if(error){
-		// 	cout << 3 << endl;
-		// 	throw boost::system::system_error(error);
-		// }
+		if(error == boost::asio::error::eof){
+			cout << 2 << endl;
+			break;
+		}else if(error){
+			cout << 3 << endl;
+			throw boost::system::system_error(error);
+		}
 
-		cout.write(buf.data(), len);
 
-		//cout << 1 << endl;
+		cout << "!!!!!!!!!!" << endl;
+		cout << buf.data() << endl;
+		cout << (int*)buf.data() << endl;
+		cout << (double*)buf.data() << endl;
+		printf("Value:  %p\n", &buf );
+		cout << "!!!!!!!!!!" << endl;
+
+		// boost::asio::async_write(socket_, boost::asio::buffer(message_),
+	 //        boost::bind(&tcp_connection::handle_write, shared_from_this(),
+	 //          boost::asio::placeholders::error,
+	 //          boost::asio::placeholders::bytes_transferred));
+
+		//cout.write(buf.data(), len);
+
+		cout << "loop end" << endl; //del
 	}
 
 
