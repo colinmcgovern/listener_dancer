ec3de784923cd77177f2ab9405140ad6db1c5601
Colin McGovern <thecolinmcgovern@gmail.com>
2021-03-24 19:29:37 -0400

    network controlled cubegit add *

---
+++ cube_and_job_handler.cpp
@@ -0,0 +1,263 @@
+#include <ctime>
+#include <iostream>
+#include <string>
+#include <boost/bind/bind.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/enable_shared_from_this.hpp>
+#include <boost/asio.hpp>
+#include <boost/array.hpp>
+#include <GL/glut.h>
+
+//g++ cube_and_job_handler.cpp -pthread -lboost_thread -lglut -lGL -lGLU -o cube_and_job_handler
+
+using boost::asio::ip::tcp;
+using namespace std;
+using std::vector;
+
+float ver[8][3] = 
+{
+    {-1.0,-1.0,1.0},
+    {-1.0,1.0,1.0},
+    {1.0,1.0,1.0},
+    {1.0,-1.0,1.0},
+    {-1.0,-1.0,-1.0},
+    {-1.0,1.0,-1.0},
+    {1.0,1.0,-1.0},
+    {1.0,-1.0,-1.0},
+};
+
+GLfloat color[8][3] = 
+{
+    {0.0,0.0,0.0},
+    {1.0,0.0,0.0},
+    {1.0,1.0,0.0},
+    {0.0,1.0,0.0},
+    {0.0,0.0,1.0},
+    {1.0,0.0,1.0},
+    {1.0,1.0,1.0},
+    {0.0,1.0,1.0},
+};
+
+void quad(int a,int b,int c,int d)
+{
+    glBegin(GL_QUADS);
+    glColor3fv(color[a]);
+    glVertex3fv(ver[a]);
+
+    glColor3fv(color[b]);
+    glVertex3fv(ver[b]);
+
+    glColor3fv(color[c]);
+    glVertex3fv(ver[c]);
+
+    glColor3fv(color[d]);
+    glVertex3fv(ver[d]);
+    glEnd();
+}
+
+void colorcube()
+{
+    quad(0,3,2,1);
+    quad(2,3,7,6);
+    quad(0,4,7,3);
+    quad(1,2,6,5);
+    quad(4,5,6,7);
+    quad(0,1,5,4);
+}
+
+double rotate_y = 0; 
+double rotate_x = 0;
+void specialKeys( int key, int x, int y ) 
+{
+    if (key == GLUT_KEY_RIGHT)
+        rotate_y += 5;
+    else if (key == GLUT_KEY_LEFT)
+        rotate_y -= 5;
+    else if (key == GLUT_KEY_UP)
+        rotate_x += 5;
+    else if (key == GLUT_KEY_DOWN)
+        rotate_x -= 5;
+    glutPostRedisplay();
+}
+
+void display()
+{
+    glClearColor( 0, 0, 0, 1 );
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+
+    glMatrixMode( GL_PROJECTION );
+    glLoadIdentity();
+    int w = glutGet( GLUT_WINDOW_WIDTH );
+    int h = glutGet( GLUT_WINDOW_HEIGHT );
+    gluPerspective( 60, w / h, 0.1, 100 );
+
+    glMatrixMode( GL_MODELVIEW );
+    glLoadIdentity();
+    gluLookAt
+        ( 
+        3, 3, 3, 
+        0, 0, 0,
+        0, 0, 1
+        );
+
+    glRotatef( rotate_x, 1.0, 0.0, 0.0 );
+    glRotatef( rotate_y, 0.0, 1.0, 0.0 );
+    colorcube();
+
+    glutSwapBuffers();
+}
+
+void timer(int extra)
+{
+    //rotate_y += 5;
+    glutTimerFunc(30, timer, 0);
+    glutPostRedisplay();
+}
+
+class tcp_connection : public boost::enable_shared_from_this<tcp_connection> {
+	public: 
+		typedef boost::shared_ptr<tcp_connection> pointer;
+
+		static pointer create(boost::asio::io_context& io_context){
+			cout << "tcp_connection::pointer()" << endl; //del
+			return pointer(new tcp_connection(io_context));
+		}
+
+		tcp::socket& socket(){
+			cout << "socket()" << endl; //del
+			return socket_;
+		}
+
+		void start(){
+			cout << "start() started" << endl; //del
+
+			cout << "Sending info to " <<
+			 socket_.remote_endpoint().address().to_string() << ":" << 
+			 socket_.remote_endpoint().port() <<
+			endl;
+
+			for(;;){
+				cout << "loop start" << endl; //del
+
+				boost::array<char,128> buf;
+				boost::system::error_code error;
+				
+				size_t len = socket_.read_some(boost::asio::buffer(buf), error);
+
+				if(error == boost::asio::error::eof){
+					cout << 2 << endl;
+					break;
+				}else if(error){
+					cout << 3 << endl;
+					throw boost::system::system_error(error);
+				}
+
+				cout << "!!!!!!!!!!" << endl;
+				//cout << len << endl;
+				cout << buf.data() << endl;
+				rotate_y += 5;
+				cout << "!!!!!!!!!!" << endl;
+
+				//string message = "test";
+				boost::asio::async_write(socket_, boost::asio::buffer(buf),
+				boost::bind(&tcp_connection::handle_write, shared_from_this(),
+				boost::asio::placeholders::error,
+				boost::asio::placeholders::bytes_transferred));
+
+				cout << "loop end" << endl; //del
+			}
+
+			
+			cout << "start() ended" << endl; //del
+		}
+
+	private:
+		tcp_connection(boost::asio::io_context& io_context)
+		: socket_(io_context)
+		{
+			cout << "tcp_connection()" << endl; //del
+		}
+
+		void handle_write(const boost::system::error_code& /*error*/,
+		  size_t /*bytes_transferred*/)
+		{
+			cout << "handle_write()" << endl; //del
+		}
+
+		tcp::socket socket_;
+};
+
+
+class tcp_server{
+	public:
+		tcp_server(boost::asio::io_context& io_context) : io_context_(io_context), 
+		acceptor_(io_context, tcp::endpoint(tcp::v4(), 15)){
+			cout << "tcp_server() started" << endl; //del
+			start_accept();
+			cout << "tcp_server() ended" << endl; //del
+		}
+
+	private:
+		void start_accept(){
+
+			cout << "start_accept() started" << endl; //del
+			tcp_connection::pointer new_connection = tcp_connection::create(io_context_);
+
+			acceptor_.async_accept(new_connection->socket(),
+				boost::bind(&tcp_server::handle_accept, this, new_connection,
+					boost::asio::placeholders::error));
+
+			cout << "start_accept() ended" << endl; //del
+		}
+
+		void handle_accept(tcp_connection::pointer new_connection,
+			const boost::system::error_code& error){
+
+			cout << "handle_accept() started" << endl; //del
+
+			if(!error){
+				new_connection->start();
+			}
+
+			start_accept();
+
+			cout << "handle_accept() ended" << endl; //del
+		}
+
+		boost::asio::io_context& io_context_;
+		tcp::acceptor acceptor_;
+};
+
+
+int main( int argc, char **argv ){
+
+	cout << "main() started" << endl; //del
+
+	thread t1([&]{
+		glutInit( &argc, argv );
+	    glutInitDisplayMode( GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE );
+	    glutInitWindowSize( 640, 480 );
+	    glutCreateWindow( "GLUT" );
+	    glutDisplayFunc( display );
+	    glutSpecialFunc( specialKeys );
+	    glEnable( GL_DEPTH_TEST );
+	    glutTimerFunc(0, timer, 0);
+	    glutMainLoop();
+	});
+
+	for(;;){
+		
+		try{
+			boost::asio::io_context io_context;
+			tcp_server server(io_context);
+			io_context.run();
+		}catch(std::exception& e){
+			std::cerr << e.what() << std::endl;
+		}
+	}
+
+
+	cout << "main() ended" << endl; //del
+
+	return 0;
+}
